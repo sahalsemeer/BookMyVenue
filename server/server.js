8056da2ac02289278cb7f66a4cf4354ed55fb9b6
sahalsemeer <mohdsahalsemeer@icloud.com>
2026-06-01 11:38:08 +0530

    db config

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB()
+  .then(() => {
+    console.log("Database Connected!");
+    app.listen(5555, () => {
+      console.log("Server is Running on 5555");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
